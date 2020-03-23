43f36ef99516cef867543aec3921f94e60c78225
duyumengdym <272780382@qq.com>
2020-03-23 17:39:20 +0800

    range

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 /* global $:true*/ 
 $(function() {
-  var $btn = $('input'),
-      num =6,
-      timer;
-  timer = setInterval(function(){
-    num--;
-    if(num===0){
-      clearInterval(timer);
-      $btn.val('同意');
-      $btn.removeAttr('disabled');
-    }else{
-      $btn.val('同意('+num+'s)');
-    }
-  },1000);
-  $btn.click(function(){
-    alert('就知道你会同意的');
+  var $range = $('#range'),
+      $age =$('#age');
+  $range.change(function(){
+    $age.html($range.val());
   }); 
 });
